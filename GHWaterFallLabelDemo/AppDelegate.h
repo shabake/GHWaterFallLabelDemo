f1155938ffee636544087bed161aef286a03c37e
shabake <45329453@qq.com>
2019-02-16 16:11:10 +0800

    初始化项目

---
+++ GHWaterFallLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHWaterFallLabelDemo
+//
+//  Created by zhaozhiwei on 2019/2/16.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHWaterFallLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHWaterFallLabelDemo
+//
+//  Created by zhaozhiwei on 2019/2/16.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
